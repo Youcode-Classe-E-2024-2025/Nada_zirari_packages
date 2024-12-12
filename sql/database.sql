0528331d8a3b8d2088db75f2275ac3c358d10664
Nada Zirari <zirarinada20@gmail.com>
2024-12-12 23:12:56 +0100

    creation du table pavckages

--- sql/database.sql
+++ sql/database.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE packages;
 
+USE packages;
+
+CREATE TABLE package (
+    id_package INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
