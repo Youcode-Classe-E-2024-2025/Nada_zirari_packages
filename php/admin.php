d00f8f999afaba913cc375db8bd5661c500ed004
Nada Zirari <zirarinada20@gmail.com>
2024-12-12 22:57:17 +0100

    ajout du forms for author

--- php/admin.php
+++ php/admin.php
@@ -78,6 +78,31 @@
                 </form>
             </div>
 
+            <!-- Formulaire pour Auteur -->
+            <div id="formulaire-auteur" class="max-w-xl mx-auto bg-white rounded-xl shadow-lg p-8 hidden">
+                <h2 class="text-3xl font-extrabold text-indigo-600 mb-6 text-center">Ajouter un Auteur</h2>
+                <form action="" method="POST" class="space-y-6">
+                    <input type="hidden" name="type" value="auteur">
+                    <div>
+                        <label for="nom_Auteur" class="block text-sm font-medium text-gray-700 mb-1">Nom</label>
+                        <input type="text" name="nom_Auteur" id="nom_Auteur" placeholder="Entrez le nom de l'auteur"
+                               class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm" required>
+                    </div>
+                    <div>
+                        <label for="email_Auteur" class="block text-sm font-medium text-gray-700 mb-1">Email</label>
+                        <input type="email" name="email_Auteur" id="email_Auteur" placeholder="Entrez l'email de l'auteur"
+                               class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm" required>
+                    </div>
+                    <div class="flex justify-center">
+                        <button type="submit"
+                                class="bg-green-500 hover:bg-indigo-600 text-white font-medium px-6 py-2 rounded-lg shadow-md">
+                            Ajouter l'Auteur
+                        </button>
+                    </div>
+                </form>
+            </div>
+
            
+    
 </body>
 </html>
